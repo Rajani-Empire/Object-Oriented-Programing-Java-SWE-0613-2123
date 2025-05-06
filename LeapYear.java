4f4a7d5a1ba4283a1bbb1801f82da6b03b825f0b
Rajani Empire <rajani.swe.sust@gmail.com>
2025-05-06 22:16:07 +0600

    jfrki

---
+++ LeapYear.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class LeapYear {
+    public static void main(String[] args) {
+
+        System.out.print("Enter the year : ");
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+
+        if( year%400==0 ||(year%4==0 && year%100 !=0 )){
+            System.out.println("Leap Year");
+        }
+        else{
+            System.out.println("No Leap Year");
+        }
+        
+    
+    }
+    
+}
