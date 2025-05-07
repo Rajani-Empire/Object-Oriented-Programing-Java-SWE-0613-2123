1010e54e469e819800e805cc1883b25190a3d9a1
Rajani Empire <rajani.swe.sust@gmail.com>
2025-05-07 19:21:57 +0600

    uui

---
+++ Reverse.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+
+        String rev = new StringBuilder(s).reverse().toString();
+
+
+        System.out.println("reverse string : "+rev);
+
+        
+    }
+}
